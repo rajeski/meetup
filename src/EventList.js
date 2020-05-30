c938ce64be0f02ab3eb8ce878ccdca600b14b82a
rajeski <asiajsr@gmail.com>
2020-05-30 17:09:07 +0900

    Edited EventList.js while troubleshooting... not a function error

--- src/EventList.js
+++ src/EventList.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 import Event from "./Event";
 
-const EventList = ({ events }) => {
-  return (
-    <ul className="EventList">
-      {events.map((event) => (
-        <li key={event.id}>
-          <Event event={event} />
-        </li>
-      ))}
-    </ul>
-  );
-};
+class EventList extends Component {
+  render() {
+    return (
+      <ul className="EventList">
+        if!(events) return null
+        {this.props.events.map((event) => (
+          <li key={event.id}>
+            <Event event={event} />
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
 
 export default EventList;
