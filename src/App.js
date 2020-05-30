1749e50628114e6d395232db23bdabb1d450e07a
rajeski <asiajsr@gmail.com>
2020-05-30 19:47:06 +0900

    Troubleshooting CORS error related edits

--- src/App.js
+++ src/App.js
@@ -1,129 +1,109 @@
-import React, { Component } from "react";
-import moment from "moment";
-import "./App.css";
-import {
-  ScatterChart,
-  Scatter,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  ResponsiveContainer,
-} from "recharts";
-
-import EventList from "./EventList";
-import CitySearch from "./CitySearch";
-import NumberOfEvents from "./NumberOfEvents";
-import { getEvents } from "./api";
+import React, { Component } from 'react';
+import './App.css';
+import EventList from './EventList';
+import CitySearch from './CitySearch';
+import NumberOfEvents from './NumberOfEvents';
+import { getEvents } from './api';
+import moment from 'moment'
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts'
 
 class App extends Component {
-  componentDidMount() {
-    getEvents().then((response) => this.setState({ events: response }));
-  }
-
   state = {
     events: [],
     page: null,
-    defaultCity: "",
+    defaultCity: '',
     lat: null,
     lon: null,
-    warningText: "",
-  };
+  }
+
+  componentDidMount() {
+    getEvents()
+      .then(response => {
+        console.log('test');
+        console.log(response.events);
+        this.setState({ events: response })
+      });
+  }
 
   updateEvents = (lat, lon, page) => {
-    if (!navigator.onLine) {
-      this.setState({
-        warningText: "Network Offline! Event list loaded from last session.",
-      });
-    } else {
-      this.setState({ warningText: "" });
-    }
-
     if (lat && lon) {
-      getEvents(lat, lon, this.state.page).then((response) =>
-        this.setState({ events: response, lat, lon })
-      );
-    } else if (page) {
-      getEvents(this.state.lat, this.state.lon, page).then((response) =>
-        this.setState({ events: response, page })
-      );
-    } else {
-      getEvents(
-        this.state.lat,
-        this.state.lon,
-        this.state.page
-      ).then((response) => this.setState({ events: response }));
+
+      getEvents(lat, lon, this.state.page).then(response => this.setState({ events: response, lat, lon }));
     }
-  };
+    else if (page) {
 
-  countEventsOnADate = (date) => {
+      getEvents(this.state.lat, this.state.lon, page).then(response => this.setState({ events: response, page }));
+    }
+    else {
+
+      getEvents(this.state.lat, this.state.lon, this.state.page).then(response => this.setState({ events: response }));
+    }
+  }
+
+  countEventsOnADate = date => {
+    if (this.state.events.events === undefined) return 0;
     let count = 0;
-
-    for (let i = 0; i < this.state.events.length; i += 1) {
-      if (this.state.events[i].local_date === date) {
+    for (let i = 0; i < this.state.events.events.length; i += 1) {
+      if (this.state.events.events[i].local_date === date) {
         count += 1;
       }
-    }
 
+    }
     return count;
   };
 
+
   getData = () => {
-    const next7Days = [];
-    const currentDate = moment();
+    console.log(this.state.events, 2)
+    const next7Days = [];  // Create empty seven day array
+    const currentDate = moment();  // Today
+    // Advance for the next seven days
 
     for (let i = 0; i < 7; i += 1) {
-      currentDate.add(1, "days");
-      const dateString = currentDate.format("MM-DD-YYYY");
-      const count = this.countEventsOnADate(dateString);
-      next7Days.push({ date: dateString, number: count });
-    }
+      const dateString = currentDate.format('MM-DD-YYYY'); // US date format
+      console.log(dateString)
 
-    return next7Days;
-  };
+
+      const count = this.countEventsOnADate(dateString);
+      console.log(count) // Use countEventsOnADate function to count number of events on a given date
+      next7Days.push({ date: dateString, number: count }); // Add the specific date and number to the display list
+      currentDate.add(1, 'days'); // Add one day to current date, currentDate changes
+    }
+    return next7Days
+  }
 
   render() {
+    // console.log('test', this.state)
     return (
+
       <div className="App">
-        <div className="headLine">
-          <h1>
-            <span>Upcoming MeetUps</span>
-          </h1>
-        </div>
         <CitySearch updateEvents={this.updateEvents} />
-        <NumberOfEvents
+        {this.state.events && <NumberOfEvents
           updateEvents={this.updateEvents}
           numberOfEvents={this.state.events.length}
           lat={this.state.lat}
-          lon={this.state.lon}
-        />
-        <div className="chartWrapper">
-          <ResponsiveContainer height={250}>
-            <ScatterChart
-              margin={{
-                top: 20,
-                right: 20,
-                bottom: 20,
-                left: -20,
-              }}
-            >
-              <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
-              <XAxis type="category" dataKey="date" name="date" />
-              <YAxis
-                type="number"
-                dataKey="number"
-                name="number of events"
-                allowDecimals={false}
-              />
-              <Tooltip cursor={{ strokeDasharray: "3 3" }} />
-              <Scatter data={this.getData()} fill="#8884d8" />
-            </ScatterChart>
-          </ResponsiveContainer>
-        </div>
-        <EventList events={this.state.events} />
+          lon={this.state.lon} />}
+
+        <ResponsiveContainer height={400} >
+          <ScatterChart
+            margin={{
+              top: 20, right: 20, bottom: 20, left: 20,
+            }}
+          >
+            <CartesianGrid />
+            <XAxis type="category" dataKey="date" name="date" />
+            <YAxis type="number" dataKey="number" name="number of events" />
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+            <Scatter data={this.getData()} fill="#8884d8" />
+          </ScatterChart>
+
+        </ResponsiveContainer>
+
+        {console.log(this.state.events, 1)}
+        {this.state.events.events && <EventList events={this.state.events.events} />}
+
       </div>
     );
   }
 }
-
 export default App;
--- src/Event.js
+++ src/Event.js
@@ -1,109 +1,65 @@
-import React, { Component } from "react";
-import {
-  PieChart,
-  Pie,
-  Tooltip,
-  Cell,
-  Legend,
-  ResponsiveContainer,
-} from "recharts";
+import React, { Component } from 'react';
+import { PieChart, Pie, Tooltip, Cell, Legend, ResponsiveContainer } from 'recharts';
 
 class Event extends Component {
   state = {
-    showDetails: false,
-  };
+    showDetails: false
+  }
 
   handleShowDetails = () => {
     if (this.state.showDetails === false) {
       this.setState({ showDetails: true });
-    } else {
+    }
+    else {
       this.setState({ showDetails: false });
     }
-  };
+  }
 
   render() {
     const showDetails = this.state.showDetails;
     const event = this.props.event;
-    const data = [
-      { name: "Attending", value: event.yes_rsvp_count },
-      { name: "Available", value: event.rsvp_limit - event.yes_rsvp_count },
-    ];
+    const data = [{ name: "Attending", value: event.yes_rsvp_count }, { name: "Available", value: (event.rsvp_limit - event.yes_rsvp_count) }];
     const colors = ["#8884d8", "#37c0ba"];
 
     return (
       <div className="event">
         <div className="event__Overview">
-          <p className="event__Overview--name">{this.props.event.name}</p>
-          <p className="event__Overview--localDate">
-            {this.props.event.local_date}
-          </p>
-          <p className="event__Overview--groupName">
-            {this.props.event.group.name}
-          </p>
-          {this.props.event.venue && (
-            <p className="event__Overview--venue">
-              @{this.props.event.venue.name} |{" "}
-              {this.props.event.venue.address_1} | {this.props.event.venue.city}
-            </p>
-          )}
-          {event.rsvp_limit && (
+          <p className="event__Overview--localDate">{this.props.event.local_date} | {this.props.event.local_time}</p>
+          <h2 className="event__Overview--name">{this.props.event.name}</h2>
+          <p className="event__Overview--groupName">{this.props.event.group.name}</p>
+          {this.props.event.venue &&
+            <p className="event__Overview--venue">@{this.props.event.venue.name} | {this.props.event.venue.address_1} | {this.props.event.venue.city}</p>
+          }
+          {event.rsvp_limit &&
             <ResponsiveContainer height={150} width={250}>
               <PieChart>
-                <Pie
-                  data={data}
-                  dataKey="value"
-                  nameKey="name"
-                  cx="50%"
-                  cy="50%"
-                  outerRadius={32}
-                  label
-                >
-                  {data.map((entry, index) => (
-                    <Cell key={`cell-${index}`} fill={colors[index]} />
-                  ))}
+                <Pie data={data} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={32} label >
+                  {
+                    data.map((entry, index) => (<Cell key={`cell-${index}`} fill={colors[index]} />))
+                  }
                 </Pie>
-                <Legend
-                  iconSize={10}
-                  iconType="triangle"
-                  layout="horizontal"
-                  verticalAlign="bottom"
-                  align="center"
-                />
+                <Legend iconSize={10} iconType="triangle" layout="horizontal" verticalAlign="bottom" align="center" />
                 <Tooltip />
               </PieChart>
             </ResponsiveContainer>
-          )}
-          {!event.rsvp_limit && <p>{event.yes_rsvp_count} People Attending</p>}
-          {showDetails && (
-            <button
-              className="details-btn"
-              onClick={() => this.handleShowDetails()}
-            >
-              hide details
-            </button>
-          )}
-          {!showDetails && (
-            <button
-              className="details-btn"
-              onClick={() => this.handleShowDetails()}
-            >
-              show details
-            </button>
-          )}
+          }
+          {!event.rsvp_limit &&
+            <p>{event.yes_rsvp_count} People Joining</p>
+          }
+          {showDetails &&
+            <button className="details-btn" onClick={() => this.handleShowDetails()}>hide details</button>
+          }
+          {!showDetails &&
+            <button className="details-btn" onClick={() => this.handleShowDetails()}>show details</button>
+          }
         </div>
-        {showDetails && (
+        {showDetails &&
           <div className="event__Details">
-            <h3>Info</h3>
-            <h4>
-              <a href={this.props.event.link} target="blank">
-                Go To MeetUp
-              </a>
-            </h4>
-            <p className="event__Details--description">
-              {this.props.event.description}
-            </p>
+            <h3>Infos</h3>
+            <h4><a href={this.props.event.link} target="blank">Attend MeetUp</a></h4>
+            <p className="event__Details--description" dangerouslySetInnerHTML={{ __html: this.props.event.description }} />
           </div>
-        )}
+        }
       </div>
     );
   }
