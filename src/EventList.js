ae7e35c493d541f7c256d736b6be3d3204830eca
rajeski <asiajsr@gmail.com>
2020-05-30 16:05:27 +0900

    Updated EventList.js while troubleshooting TypeError

--- src/EventList.js
+++ src/EventList.js
@@ -1,18 +1,18 @@
-import React from "react";
-
+import React, { Component } from "react";
 import Event from "./Event";
 
-const EventList = ({ events }) => {
-  return (
-    <ul className="EventList">
-      {console.log(events)}
-      {events.map((event) => (
-        <li key={event.id}>
-          <Event event={event} />
-        </li>
-      ))}
-    </ul>
-  );
-};
+class EventList extends Component {
+  render() {
+    return (
+      <ul className="EventList">
+        {this.props.events.map((event) => (
+          <li key={event.id}>
+            <Event event={event} />
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
 
 export default EventList;
